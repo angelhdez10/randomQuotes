77790bf853f5047dcaf17a8774a29d4bf0744094
Angel Hernandez <angelhdez416@gmail.com>
2022-04-09 12:45:58 -0500

    randomQuotes done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Quote from './components/Quote';
+import {useState} from 'react';
+
+const theme = [
+  "#E83845",
+  "#746AB0",
+  "E389B9",
+  "#FFCD00",
+  "#00A8E8",
+  "288BA8",
+  "#4B0082",
+  "#000080"
+]
+
+const quotes = [
+  {quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", author: "Marilyn Monroe"},
+  {quote: "Be yourself; everyone else is already taken.", author: "Oscar Wilde"},
+  {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein"},
+  {quote: "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.", author: "Bernard M. Baruch"},
+  {quote: "You've gotta dance like there's nobody watching", author: "William W. Purkey"},
+  {quote: "You know you're in love when you can't fall asleep because reality is finally better than your dreams", author: "Dr. Seuss"},
+  {quote: "A room without books is like a body without a soul.", author: "Marcus Tullius Cicero"},
+  {quote: "You only live once, but if you do it right, once is enough.", author: "Mae West"},
+  {quote: "Be the change that you wish to see in the world.", author: "Mahatma Gandhi"},
+]
+let indice =0
+const AppContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  background-color: ${props => props.theme};
+  transition: background-color 1s ease-in-out;
+
+`;
+
+const getRandom = (quote) => {
+  if(!quote){
+    return Math.floor(Math.random() * quotes.length)
+  }
+  return Math.floor(Math.random() * (quotes.length - 1))
+}
+
 
 function App() {
+  const[quote, setQuote] = useState(quotes[getRandom()])
+  const changeColor = () => {
+    console.log(indice)
+    indice = (indice + 1); 
+    return indice
+  }
+
+  const newQuote = () => {
+    let temp = quotes.filter(q => q.quote !== quote.quote)
+    setQuote(temp[getRandom(quote)])
+    indice = quotes.indexOf(quote)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer theme={theme[indice]} className="App">
+      <Quote changeColor={changeColor} quote={quote} newQuote={newQuote} theme={theme[indice]}/>
+    </AppContainer>
   );
 }
 
---
+++ src/components/Quote.js
@@ -0,0 +1,90 @@
+import styled from 'styled-components';
+
+const Div = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+`
+
+const QuoteBlock = styled.blockquote`
+  background-color: #f9f9f9;
+  height: auto;
+  width: 450px;
+  padding: 20px;
+  border-radius: 3px;
+  
+
+  blockquote {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 22px;
+    padding: 0 10px;
+  }
+
+  #text {
+    font-size: 18px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    color: ${props => props.theme};
+  }
+
+  #author {
+    color: ${props => props.theme};
+    font-size: 14px;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 300;
+    align-self: flex-end;
+  }
+
+`
+
+const NewQuote = styled.button`
+  color: white;
+  font-size: 12px;
+  height: 28px;
+  border: none;
+  padding:0 5px;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme};
+  transition: background-color 1s ease-in-out;
+`
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 30px;
+  align-items: center;
+
+  a {
+    margin: 0 5px;
+    border-radius: 3px;
+    padding: 2px 10px;
+    background-color: ${(props) => props.theme};
+    transition: background-color 1s ease-in-out;
+  }
+`;
+
+const Quote = ({quote, theme, changeColor, newQuote}) => {
+  return (
+    <QuoteBlock id="quote-box" theme={theme}>
+      <blockquote>
+        <p  id="text"><i class="fa fa-quote-left"/>{quote.quote}</p>
+        <p id="author">-{quote.author}</p>
+      </blockquote>
+      <Div>
+        <Buttons theme={theme}>
+          <a href="https://twitter.com/intent/tweet" target="_blank" id="tweet-quote" ><i class="fa-brands fa-twitter"></i></a>
+          <a href="" target="_blank" ><i class="fa-brands fa-tumblr"></i></a>
+        </Buttons>
+        <NewQuote id="new-quote" theme={theme} onClick={newQuote}>New quote</NewQuote>
+      </Div>
+    </QuoteBlock>
+  );
+}
+
+export default Quote
+
+
